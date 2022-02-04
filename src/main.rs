a90c3243dad485121058b37a1e4fa761d606f585
0xTiger <tgh.lizard@googlemail.com>
2022-02-04 22:38:12 +0000

    fix: nearly fixed lift

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,16 @@ impl Aircraft {
     fn draw(&self) {
         // sin(rot) = y_offset / 1
         // cos(rot) = x_offset
-        let inv_rot = -self.rot;
-        let x_offset = inv_rot.cos() * 10.0;
-        let y_offset = inv_rot.sin() * 10.0;
-        draw_line(self.x - x_offset, self.y - y_offset, 
-                    self.x + x_offset, self.y + y_offset, 3.0, YELLOW);
+        let x_offset = self.rot.cos() * 10.0;
+        let y_offset = self.rot.sin() * 10.0;
+
+        let x1 = self.x - x_offset;
+        let y1 = self.y - y_offset;
+        let x2 = self.x + x_offset;
+        let y2 = self.y + y_offset;
+
+        draw_line(x1, screen_height() - y1, 
+                    x2, screen_height() - y2, 3.0, YELLOW);
     }
 
     fn rotate(&mut self, amount: f32) {
@@ -29,28 +34,42 @@ impl Aircraft {
     fn update_pos(&mut self) {
         self.x += self.x_vel;
         self.y += self.y_vel;
+        
     }
 
     fn do_lift(&mut self) {
-        let lift_angle = self.rot + PI / 2.0;
-        let lift_x_vel = lift_angle.cos() * self.x_vel * 0.5;
-        let lift_y_vel = lift_angle.sin() * self.y_vel * 0.5;
-        draw_text(lift_x_vel.to_string().as_str(), 20.0, 40.0, 30.0, DARKGRAY);
-        draw_text(lift_y_vel.to_string().as_str(), 20.0, 60.0, 30.0, DARKGRAY);
-        self.x_vel += lift_x_vel;
-        self.y_vel += lift_y_vel;
+        let lift_dir = self.rot + PI / 2.0;
+        let down_dir = self.rot - PI / 2.0;
+        let vel_dir = dir(self.y_vel, self.x_vel);
+
+        let mag_in_down_dir = mag(self.x_vel, self.y_vel) * (vel_dir - down_dir).abs().cos();
+        let lift_x_accel = lift_dir.cos() * mag_in_down_dir * 0.1;
+        let lift_y_accel = lift_dir.sin() * mag_in_down_dir * 0.1;
+
+
+        draw_text(("lift_x_accel: ".to_owned() + lift_x_accel.to_string().as_str()).as_str(), 20.0, 30.0, 20.0, DARKGRAY);
+        draw_text(("lift_y_accel: ".to_owned() + lift_y_accel.to_string().as_str()).as_str(), 20.0, 45.0, 20.0, DARKGRAY);
+        self.x_vel += lift_x_accel;
+        self.y_vel += lift_y_accel;
     }
         
 }
 
+fn dir(x: f32, y: f32) -> f32 {
+    (x / y).atan()
+}
+
+fn mag(x: f32, y: f32) -> f32 {
+    (x.powi(2) + y.powi(2)).sqrt()
+}
 #[macroquad::main("L2F")]
 async fn main() {
 
     let mut myplane = Aircraft { x: screen_width() / 2.0, 
-                                y: screen_height() / 2.0,
+                                y: screen_height() / 1.1,
                                 rot: 0.0,
                                 x_vel: 0.0,
-                                y_vel: 0.0};
+                                y_vel: -0.0};
     loop {
         clear_background(RED);
 
@@ -63,12 +82,25 @@ async fn main() {
             myplane.rotate(-0.05);
         }
 
-        myplane.draw();
-        myplane.update_pos();
-        myplane.y_vel += 0.01;
-        myplane.do_lift();
+        if is_key_down(KeyCode::Enter) {
+            myplane.x = screen_width() / 2.0;
+            myplane.y = screen_height() / 2.0;
+            myplane.x_vel = 0.0;
+            myplane.y_vel = 0.0;
+        }
 
-        draw_text(myplane.rot.to_string().as_str(), 20.0, 20.0, 30.0, DARKGRAY);
+        myplane.draw();
+        // Gravity
+        myplane.y_vel -= 0.01;
+        // Air resistance
+        // myplane.y_vel *= 0.98;
+        // myplane.x_vel *= 0.98;
+        myplane.do_lift();
+        
+        myplane.update_pos();
+        draw_text(("x_vel: ".to_owned() + myplane.x_vel.to_string().as_str()).as_str(), 20.0, 60.0, 20.0, DARKGRAY);
+        draw_text(("y_vel: ".to_owned() + myplane.y_vel.to_string().as_str()).as_str(), 20.0, 75.0, 20.0, DARKGRAY);
+        draw_text(myplane.rot.to_string().as_str(), 20.0, 15.0, 20.0, DARKGRAY);
         next_frame().await
     }
 }
